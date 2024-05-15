2a65cf9f85921545bd8f941d638bc934e821aeec
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-05-15 11:17:59 +0900

    サービスの流れスライド修正

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -17,14 +17,13 @@ function initializeSlider() {
         arrows: true,
         variableWidth: true,
         centerMode: true,
-        centerMode: true,
         centerPadding: "15%",
         responsive: [
             {
                 breakpoint: 767, // 399px以下のサイズに適用
                 settings: {
                     centerPadding: "0%",
-                    slidesToShow: 1
+                    slidesToShow: 1,
                 },
             },
         ],
@@ -104,6 +103,39 @@ $(document).ready(function () {
     });
 
     //---------------------------service---------------------------
+    const $slider = $(".js-slide--service");
+
+    // 左右の透過の2周目ががたつく対応
+    $slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+        $slider.find(".slick-slide").each((index, el) => {
+        const $this = $(el),
+            slickindex = $this.attr("data-slick-index");
+        if (nextSlide == slick.slideCount - 1 && currentSlide == 0) {
+            // 現在のスライドが最初のスライドでそこから最後のスライドに戻る場合
+            if (slickindex == "-1") {
+            // 最後のスライドに対してクラスを付与
+            $this.addClass("is-active-next");
+            } else {
+            // それ以外は削除
+            $this.removeClass("is-active-next");
+            }
+        } else if (nextSlide == 0) {
+            // 次のスライドが最初のスライドの場合
+            if (slickindex == slick.slideCount) {
+            // 最初のスライドに対してクラスを付与
+            $this.addClass("is-active-next");
+            } else {
+            // それ以外は削除
+            $this.removeClass("is-active-next");
+            }
+        } else {
+            // それ以外は削除
+            $this.removeClass("is-active-next");
+        }
+        });
+    });
+
+
     //ダンボール紹介のスライド
     $('.size__tags .tag, .size__pagenation .pagenation').on('click', function(){
         if(!$(this).hasClass('active')) {
