f0bfadefacd58eca18fada54dadc0987640638a3
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-06-07 18:32:12 +0900

    アニメーション追加

--- src/assets/js/anime.js
+++ src/assets/js/anime.js
@@ -1,28 +1,52 @@
 $(document).ready(function() {
     // Function to check if an element is in viewport
+    //スクロール表示タイミング
     function isInViewport(element) {
         var elementTop = $(element).offset().top;
         var elementBottom = elementTop + $(element).outerHeight();
         var viewportTop = $(window).scrollTop();
         var viewportBottom = viewportTop + $(window).height();
-        return elementBottom > viewportTop && elementTop < viewportBottom;
+        return elementBottom > viewportTop + 400 && elementTop + 400 < viewportBottom;
+    }
+
+    //スクロールアニメーションのトリガー設定
+    function triggerAnime() {
+        const animeData = [
+            {animeType: "slide-in", animeState: "visible", initial: ""},
+            {animeType: "popup__trigger",animeState: "popup", initial: "opacity: 0"},
+        ]
+    
+        // 作成した連想配列をループで回す
+        $.each(animeData, function(index, value) {
+            // チーム名を変数に入れる
+            var animeType = value.animeType;
+            var animeState = value.animeState;
+            var initial = value.initial;
+            $('[data-anime="'+ animeType + '"]').each(function() {
+                if (isInViewport(this)) {
+                    $(this).addClass(animeState);
+                }
+            });
+        });
     }
 
     // Check elements on page load
     $(window).on('load scroll', function() {
-        $('.slide-in').each(function() {
-            if (isInViewport(this)) {
-                $(this).addClass('visible');
-            }
-        });
+        triggerAnime();
+        // $('.slide-in').each(function() {
+        //     if (isInViewport(this)) {
+        //         $(this).addClass('visible');
+        //     }
+        // });
 
-        $('.popup__trigger').each(function() {
-            if (isInViewport(this)) {
-                $(this).addClass('popup');
-            } else {
-                $(this).removeClass('popup');
-            }
-        });
+        // $('.popup__trigger').each(function() {
+        //     if (isInViewport(this)) {
+        //         $(this).addClass('popup');
+        //     } else {
+        //         $(this).removeClass('popup');
+        //     }
+        // });
+
     });
 });
 
