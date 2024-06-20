3f36b87f96b19f27c3ba4d160f03e321edd0c42f
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-06-20 16:58:23 +0900

    タイトルアニメーション・ローディングアニメーション

--- src/assets/js/loading.js
+++ src/assets/js/loading.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
 
     if (totalImages === 0) {
         // 画像がない場合はすぐにローディング画面を閉じる
-        // $('#loading').addClass('slide-up');
-        // $('#loading').fadeOut();
+        $('#loading').addClass('slide-up');
         setTimeout(function() {
-            $('#loading').fadeOut();
+            $('#loading').hide();
+            setTimeout(function() {
+                // サイト表示時アニメーション
+                $('.mainTitle__img--speech').addClass('is-visible');
+                $('.mainTitle__img--title').addClass('is-visible');
+            }, 400);
         }, 600); // 0.6秒待つ
         return;
     }
@@ -18,19 +22,20 @@ $(document).ready(function() {
         let img = new Image();
         img.src = $(this).attr('src');
         img.onload = img.onerror = function() {
-            //現在の読み込み状況を反映
             loadedImages++;
-            let progress = Math.floor((loadedImages / totalImages) * 100);
-            $('#progress').text(progress + '%');
-            $('#progress-bar').css('width', progress + '%');
-            
+
             if (loadedImages === totalImages) {
                 setTimeout(function() {
-                    // $('#loading').addClass('slide-up');
-                    // $('#loading').fadeOut();
+                    $('#loading h2').fadeOut();
+                    $('.loading__inner').fadeOut();
                     setTimeout(function() {
-                        $('#loading').fadeOut();
-                    }, 600); // 0.6秒待つ
+                        $('#loading').addClass('slide-up');
+                        setTimeout(function() {
+                            // サイト表示時アニメーション
+                            $('.mainTitle__img--speech').addClass('is-visible');
+                            $('.mainTitle__img--title').addClass('is-visible');
+                        }, 400);
+                    }, 400); // 0.6秒待つ
                 }, 600); // 0.6秒待つ
             }
         };
