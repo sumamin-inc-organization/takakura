96d9eb6c776e6bd4e5b108ac1b652747420bc86e
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-06-24 17:35:16 +0900

    全ページ基本アニメーション追加

--- src/assets/js/anime.js
+++ src/assets/js/anime.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     //一文字ずつ出現させる
     delayLetter();
+
+    //初期アニメーション
+    const initialData = [
+        {animeType: "initial-visible", initialCss: "opacity", initialValue: "0"},
+        {animeType: "initial-popup", initialCss: "opacity", initialValue: "0"},
+        {animeType: "initial-slideVisible", initialCss: "overflow", initialValue: "hidden"},
+    ]
+
+    //delayアニメーション
+    const delayData = [
+        {animeType: "delay-visible", initialCss: "opacity", initialValue: "0"},
+        {animeType: "delay-popup", initialCss: "opacity", initialValue: "0"},
+    ]
+
+    initialAnime(initialData);
+
     // Function to check if an element is in viewport
     //スクロール表示タイミング
     function isInViewport(element) {
@@ -27,7 +43,7 @@ $(document).ready(function() {
     * animeStage: つけたいアニメーションのクラス
     * initial: 初期の状態
     */
-    const animeData = [
+    const inviewData = [
         {animeType: "slide-inRight", initialCss: "opacity", initialValue: "0"},
         {animeType: "slide-inLeft", initialCss: "opacity", initialValue: "0"},
         {animeType: "popup", initialCss: "opacity", initialValue: "0"},
@@ -54,7 +70,7 @@ $(document).ready(function() {
         });
     }
 
-    $.each(animeData, function(index, value) {
+    $.each(inviewData, function(index, value) {
         var animeType = value.animeType;
         var initialCss = value.initialCss;
         var initialValue = value.initialValue;
@@ -65,11 +81,42 @@ $(document).ready(function() {
         });
     });
 
+    //子要素を時間差でアニメーションさせる
+    function delayAnime(data) {
+        var time = 0.3;//遅延時間を増やす秒数の値
+        var seconds = time;
+        $.each(data, function(index, value) {
+            var animeType = value.animeType;
+            $('[data-anime="'+ animeType + '"]').each(function() {
+                if (isInViewport(this)) {
+                    childs = $(this).children();
+                    $(childs).each(function () {
+                        $(this).css("transition-delay", seconds + "s");
+                        $(this).addClass(animeType);
+                        seconds = seconds + time;//delay時間を増加させる
+                    });
+                }
+            });
+        });
+    }
+
     // Check elements on page load
     $(window).on('load scroll', function() {
-        triggerAnime(animeData);
+        triggerAnime(inviewData);
+        delayAnime(delayData);
     });
 });
+
+//ページに最初に来たときに発火させるアニメーション
+function initialAnime(data) {
+    $.each(data, function(index, value) {
+        var animeType = value.animeType;
+        $('[data-anime="'+ animeType + '"]').each(function() {
+            $(this).addClass(animeType);
+            console.log($(this));
+        });
+    });
+}
 
 function fadeUpAnime() {
     // ふわっと出現させる
--- src/assets/js/loading.js
+++ src/assets/js/loading.js
@@ -1,21 +1,41 @@
-// ドキュメントの読み込みが完了したときに実行される関数
 $(document).ready(function() {
     let images = $('img');
     let totalImages = images.length;
     let loadedImages = 0;
+    let startTime = new Date().getTime();
+
+    function hideLoading() {
+        setTimeout(function() {
+            $('.loading__inner').fadeOut();
+            setTimeout(function() {
+                $('#loading').addClass('slide-up');
+                setTimeout(function() {
+                    // サイト表示時アニメーション
+                    $('.mainTitle__img--speech').addClass('is-visible');
+                    $('.mainTitle__img--title').addClass('is-visible');
+                }, 400); // 0.4秒待つ
+            }, 400); // 0.4秒待つ
+        }, 600); // 0.6秒待つ
+    }
 
     if (totalImages === 0) {
-        // 画像がない場合はすぐにローディング画面を閉じる
-        $('#loading').addClass('slide-up');
-        setTimeout(function() {
-            $('#loading').hide();
-            setTimeout(function() {
-                // サイト表示時アニメーション
-                $('.mainTitle__img--speech').addClass('is-visible');
-                $('.mainTitle__img--title').addClass('is-visible');
-            }, 400);
-        }, 600); // 0.6秒待つ
+        // 画像がない場合は最低3秒表示
+        let elapsedTime = new Date().getTime() - startTime;
+        let remainingTime = 1800 - elapsedTime;
+
+        setTimeout(hideLoading, remainingTime > 0 ? remainingTime : 0);
         return;
+        // 画像がない場合はすぐにローディング画面を閉じる
+        // $('#loading').addClass('slide-up');
+        // setTimeout(function() {
+        //     $('#loading').hide();
+        //     setTimeout(function() {
+        //         // サイト表示時アニメーション
+        //         $('.mainTitle__img--speech').addClass('is-visible');
+        //         $('.mainTitle__img--title').addClass('is-visible');
+        //     }, 400);
+        // }, 600); // 0.6秒待つ
+        // return;
     }
 
     images.each(function() {
@@ -25,18 +45,10 @@ $(document).ready(function() {
             loadedImages++;
 
             if (loadedImages === totalImages) {
-                setTimeout(function() {
-                    $('#loading h2').fadeOut();
-                    $('.loading__inner').fadeOut();
-                    setTimeout(function() {
-                        $('#loading').addClass('slide-up');
-                        setTimeout(function() {
-                            // サイト表示時アニメーション
-                            $('.mainTitle__img--speech').addClass('is-visible');
-                            $('.mainTitle__img--title').addClass('is-visible');
-                        }, 400);
-                    }, 400); // 0.6秒待つ
-                }, 600); // 0.6秒待つ
+                let elapsedTime = new Date().getTime() - startTime;
+                let remainingTime = 1800 - elapsedTime;
+
+                setTimeout(hideLoading, remainingTime > 0 ? remainingTime : 0);
             }
         };
     });
